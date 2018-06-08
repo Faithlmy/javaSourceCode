b8eb14b24394c45a96ed484dae9879f6181bf95e
root <root@localhost.localdomain>
2018-06-08 10:13:56 -0400

    add

---
+++ src/vic/java/lang/vicString.java
@@ -0,0 +1,9 @@
+package vic.java.lang;
+
+public class vicString {
+	public static void main(String[] args) {
+		char ch[] = {'a', 'b', 'c'};
+		String res = new String(ch);
+		System.out.println(res);
+	}
+}
