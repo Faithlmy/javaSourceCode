2b2c900231f642fe8050ad106f26a1b260e3478b
Faithlmy <lmengyy@126.com>
2018-06-30 22:26:37 +0800

    character.class

--- src/com/Math/TestMath.java
+++ src/com/Math/TestMath.java
@@ -1,9 +1,32 @@
 package com.Math;
 
-public class TestMath {
+import java.lang.Character.UnicodeBlock;
+
+public class TestMath  {
 
 	public static void main(String[] args)
 	{
-		System.out.println(Math.abs(-9.06));
+//		Math m = new Math();
+//		System.out.println(Math.abs(Math.sin(30)));
+
+		// my final and static
+//		myStaticAndFinal fc = new myStaticAndFinal();
+//		System.out.println(myStaticAndFinal.MyAll());
+//		System.out.println(Math.class);
+//		System.out.println(Boolean.compare(true, false));
+//		char b = 't';
+////		System.out.println(Character.hashCode(b));
+//		
+//		System.out.println(Character.BYTES);
+//		
+//		Character c  = new Character(b);
+//		System.out.println(c.hashCode());
+//		System.out.println(UnicodeBlock.ARROWS.toString());
+		TestMath t = new TestMath();
+		System.out.println(t.hashCode());
+		
 	}
+    public final int hashCode() {
+        return super.hashCode();
+    }
 }
