e80614dc4c7374b6c78f2b1fb98cde34dca6a291
lmy <lmengyy@126.com>
2022-05-05 23:21:26 +0800

    add sort

--- src/main/com/vic/dataStructure/sort/AbstrctSort.java
+++ src/main/com/vic/dataStructure/sort/AbstrctSort.java
@@ -2,4 +2,5 @@ package com.vic.dataStructure.sort;
 
 public abstract class AbstrctSort {
     public abstract int[] bubleSort(int[] arr);
+    public abstract int[] selectSort(int[] arr);
 }
--- src/main/com/vic/dataStructure/sort/SortShow.java
+++ src/main/com/vic/dataStructure/sort/SortShow.java
@@ -14,4 +14,23 @@ public class SortShow extends AbstrctSort {
         }
         return arr;
     }
+
+    @Override
+    public int[] selectSort(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            //注意minIndex的取值
+            int minIndex = i;
+            //j=i;意味着i之前的数都是有序的
+            for (int j = i; j < num.length; j++) {
+                if (num[j]<num[minIndex]){
+                    minIndex = j;
+                }
+            }
+            //交换，每一次循环的i都是未排序数据的第一个
+            int temp = num[i];
+            num[i] = num[minIndex];
+            num[minIndex] = temp;
+        }
+        return num;
+    }
 }
