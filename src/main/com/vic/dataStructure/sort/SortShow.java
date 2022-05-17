e4e8d4e8a829ce46957a049f78fbd2dccf2fb73b
lmy <lmengyy@126.com>
2022-05-17 23:52:32 +0800

    QuickSort

--- src/main/com/vic/dataStructure/sort/SortShow.java
+++ src/main/com/vic/dataStructure/sort/SortShow.java
@@ -24,7 +24,7 @@ public class SortShow extends AbstrctSort {
             int minIndex = i;
             //j=i;意味着i之前的数都是有序的
             for (int j = i; j < num.length; j++) {
-                if (num[j]<num[minIndex]){
+                if (num[j] < num[minIndex]) {
                     minIndex = j;
                 }
             }
@@ -70,45 +70,42 @@ public class SortShow extends AbstrctSort {
     @Override
     public int[] HeapSort(int[] arr) {
 
-        int temp=0;
-        for(int i=arr.length/2-1;i>=0;i--){
-            adjustHeap(arr,i,arr.length);
+        int temp = 0;
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(arr, i, arr.length);
         }
         //将堆顶元素与末尾元素进行交换，此时末尾就为最大值，将最大值全放在数组最后
         //重新调整结构，使其满足堆定义，继续交换堆顶元素与当前末尾元素，反复执行调整交换步骤，使整个序列达到有序
-        for(int j=arr.length-1;j>0;j--) {
+        for (int j = arr.length - 1; j > 0; j--) {
             //交换
             temp = arr[j];
             arr[j] = arr[0];
             arr[0] = temp;
             adjustHeap(arr, 0, j);
         }
-        System.out.println("数组"+ Arrays.toString(arr));
+        System.out.println("数组" + Arrays.toString(arr));
         return new int[0];
 
     }
 
 
-
-
-
-    public static void adjustHeap(int[]arr,int i,int length){
-        int temp=arr[i];//先取出当前元素的值，保存在临时变量中
+    public static void adjustHeap(int[] arr, int i, int length) {
+        int temp = arr[i];//先取出当前元素的值，保存在临时变量中
         //开始调整
         //k=i*2+1;k是i节点的左子节点
-        for(int k=i*2+1;k<length;k=k*2+1){
-            if(k+1<length&&arr[k]<arr[k+1]){//说明左子节点的值小于右子节点的值
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
+            if (k + 1 < length && arr[k] < arr[k + 1]) {//说明左子节点的值小于右子节点的值
                 k++;//k指向右子节点
             }
-            if(arr[k]>temp){//如果子节点大于父节点
-                arr[i]=arr[k];//把较大的值赋给当前节点
-                i=k;//!!!i指向k，继续循环比较
-            }else{
+            if (arr[k] > temp) {//如果子节点大于父节点
+                arr[i] = arr[k];//把较大的值赋给当前节点
+                i = k;//!!!i指向k，继续循环比较
+            } else {
                 break;
             }
         }
         //当for循环结束后，已经将以i为父结点的最大值放在了堆顶上（局部）
-        arr[i]=temp;//将temp的值放在调整后的位置
+        arr[i] = temp;//将temp的值放在调整后的位置
     }
 
 }
