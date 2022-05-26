5874016432ae02d127f298773c67cd0bece282b1
lmy <lmengyy@126.com>
2022-05-26 23:45:43 +0800

    spring

---
+++ src/main/java/com/spring/User.java
@@ -0,0 +1,24 @@
+package com.spring;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String address;
+
+}
